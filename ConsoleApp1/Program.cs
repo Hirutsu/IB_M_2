2d7b2d0560336732f20513ba35741069fa35dd52
Hirutsu <official_hirutsu@mail.ru>
2024-09-24 23:52:18 +0400

    task3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
-﻿Task1_Run();
-Task2_Run();
+﻿using PeNet;
+
+//Task1_Run();
+//Task2_Run();
+Task3_Run();
 
 void Task1_Run()
 {
@@ -184,4 +187,88 @@ static string CalculateHash(string filePath)
     }
 }
 
+void Task3_Run()
+{
+    string directoryPath = @"C:\Users\hirutsu\Desktop\dmde-free-2.6.0.522-win32-gui";
+
+    List<string> networkFunctions =[ "DeleteIPAddress", "FreeMibTable", "GetAdaptersAddresses", "GetAnycastIpAddressEntry",
+            "GetAnycastIpAddressTable", "GetBestRoute2", "GetHostNameW", "GetIpAddrTable",
+            "GetIpStatisticsEx", "GetUnicastIpAddressTable", "IcmpCloseHandle", "IcmpCreateFile",
+            "IcmpSendEcho", "MultinetGetConnectionPerformance", "MultinetGetConnectionPerformanceW",
+            "NetAlertRaise", "NetAlertRaiseEx", "NetApiBufferAllocate", "NetApiBufferFree",
+            "NetApiBufferReallocate", "NetApiBufferSize", "NetFreeAadJoinInformation",
+            "NetGetAadJoinInformation", "NetAddAlternateComputerName", "NetCreateProvisioningPackage",
+            "NetEnumerateComputerNames", "NetGetJoinableOUs", "NetGetJoinInformation",
+            "NetJoinDomain", "NetProvisionComputerAccount", "NetRemoveAlternateComputerName",
+            "NetRenameMachineInDomain", "NetRequestOfflineDomainJoin", "NetRequestProvisioningPackageInstall",
+            "NetSetPrimaryComputerName", "NetUnjoinDomain", "NetValidateName", "NetGetAnyDCName",
+            "NetGetDCName", "NetGetDisplayInformationIndex", "NetQueryDisplayInformation",
+            "NetGroupAdd", "NetGroupAddUser", "NetGroupDel", "NetGroupDelUser", "NetGroupEnum",
+            "NetGroupGetInfo", "NetGroupGetUsers", "NetGroupSetInfo", "NetGroupSetUsers",
+            "NetLocalGroupAdd", "NetLocalGroupAddMembers", "NetLocalGroupDel", "NetLocalGroupDelMembers",
+            "NetLocalGroupEnum", "NetLocalGroupGetInfo", "NetLocalGroupGetMembers", "NetLocalGroupSetInfo",
+            "NetLocalGroupSetMembers", "NetMessageBufferSend", "NetMessageNameAdd", "NetMessageNameDel",
+            "NetMessageNameEnum", "NetMessageNameGetInfo", "NetFileClose", "NetFileEnum", "NetFileGetInfo",
+            "NetRemoteComputerSupports", "NetRemoteTOD", "NetScheduleJobAdd", "NetScheduleJobDel",
+            "NetScheduleJobEnum", "NetScheduleJobGetInfo", "GetNetScheduleAccountInformation",
+            "SetNetScheduleAccountInformation", "NetServerDiskEnum", "NetServerEnum", "NetServerGetInfo",
+            "NetServerSetInfo", "NetServerComputerNameAdd", "NetServerComputerNameDel",
+            "NetServerTransportAdd", "NetServerTransportAddEx", "NetServerTransportDel",
+            "NetServerTransportEnum", "NetWkstaTransportEnum", "NetUseAdd", "NetUseDel", "NetUseEnum",
+            "NetUseGetInfo", "NetUserAdd", "NetUserChangePassword", "NetUserDel", "NetUserEnum",
+            "NetUserGetGroups", "NetUserGetInfo", "NetUserGetLocalGroups", "NetUserSetGroups",
+            "NetUserSetInfo", "NetUserModalsGet", "NetUserModalsSet", "NetValidatePasswordPolicyFree",
+            "NetValidatePasswordPolicy", "NetWkstaGetInfo", "NetWkstaSetInfo", "NetWkstaUserEnum",
+            "NetWkstaUserGetInfo", "NetWkstaUserSetInfo", "NetAccessAdd", "NetAccessCheck",
+            "NetAccessDel", "NetAccessEnum", "NetAccessGetInfo", "NetAccessGetUserPerms",
+            "NetAccessSetInfo", "NetAuditClear", "NetAuditRead", "NetAuditWrite", "NetConfigGet",
+            "NetConfigGetAll", "NetConfigSet", "NetErrorLogClear", "NetErrorLogRead", "NetErrorLogWrite",
+            "NetLocalGroupAddMember", "NetLocalGroupDelMember", "NetServiceControl", "NetServiceEnum",
+            "NetServiceGetInfo", "NetServiceInstall", "NetWkstaTransportAdd", "NetWkstaTransportDel",
+            "NetpwNameValidate", "NetapipBufferAllocate", "NetpwPathType", "NetApiBufferFree",
+            "NetApiBufferAllocate", "NetApiBufferReallocate", "WNetAddConnection2", "WNetAddConnection2W",
+            "WNetAddConnection3", "WNetAddConnection3W", "WNetCancelConnection", "WNetCancelConnectionW",
+            "WNetCancelConnection2", "WNetCancelConnection2W", "WNetCloseEnum", "WNetCloseEnumW",
+            "WNetConnectionDialog", "WNetConnectionDialogW", "WNetConnectionDialog1", "WNetConnectionDialog1W",
+            "WNetDisconnectDialog", "WNetDisconnectDialogW", "WNetDisconnectDialog1", "WNetDisconnectDialog1W",
+            "WNetEnumResource", "WNetEnumResourceW", "WNetGetConnection", "WNetGetConnectionW",
+            "WNetGetLastError", "WNetGetLastErrorW", "WNetGetNetworkInformation", "WNetGetNetworkInformationW",
+            "WNetGetProviderName", "WNetGetProviderNameW", "WNetGetResourceInformation", "WNetGetResourceInformationW",
+            "WNetGetResourceParent", "WNetGetResourceParentW", "WNetGetUniversalName", "WNetGetUniversalNameW",
+            "WNetGetUser", "WNetGetUserW", "WNetOpenEnum", "WNetOpenEnumW", "WNetRestoreConnectionW",
+            "WNetUseConnection", "WNetUseConnectionW", "RegQueryValueExA"];
+
+    List<string> exts = ["*.acm", "*.ax", "*.cpl", "*.dll", "*.drv", "*.efi", "*.exe", "*.mui", "*.ocx", "*.scr", "*.sys", "*.tsp", 
+        "*.mun", "*.ACM", "*.AX", "*.CPL", "*.DLL", "*.DRV", "*.EFI", "*.EXE", "*.MUI", "*.OCX", "*.SCR", "*.SYS", "*.TSP", "*.MUN"];
+
+    foreach (var ext in exts)
+    {
+        foreach (var filePath in Directory.GetFiles(directoryPath, ext))
+        {
+            Console.WriteLine($"Analyzing file: {filePath}");
+            AnalyzePEFile(filePath, networkFunctions);
+        }
+    }
+}
+
+static void AnalyzePEFile(string filePath, List<string> networkFunctions)
+{
+    try
+    {
+        var peFile = new PeFile(filePath);
+        var importFunctions = peFile.ImportedFunctions;
+
+        foreach (var importFunction in importFunctions)
+        {
+            if (networkFunctions.Contains(importFunction.Name))
+            {
+                Console.WriteLine($"Found network function: {importFunction.Name} in file: {filePath}");
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error analyzing file {filePath}: {ex.Message}");
+    }
+}
 
